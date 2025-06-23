755a8f356909698c3810a9fa8cd258bef71bf4f1
WENT1X <salievyt@gmail.com>
2025-06-23 11:46:46 +0600

    Use custom color in ColorPicker.kt

---
+++ src/main/java/com/heledron/text_display_experiments/paint_program/ColorPicker.kt
@@ -0,0 +1,30 @@
+package com.heledron.text_display_experiments.paint_program
+
+import com.heledron.text_display_experiments.utilities.hsv
+import com.heledron.text_display_experiments.utilities.toHSV
+import org.bukkit.Color
+
+object ColorPicker {
+    private var selectedPrivate: Color = Color.fromRGB(255, 0, 0)
+
+    private var customcolor: String = "#ffffff"
+    var selected: Color
+        get() = selectedPrivate
+        set(color) {
+            selectedPrivate = color
+
+            val (h,s,v) = color.toHSV()
+            HuePicker.hue = h.toInt()
+            SVPicker.sv = s to v
+        }
+
+
+    init {
+        fun updateColor() {
+            selectedPrivate = hsv(HuePicker.hue.toDouble(), SVPicker.sv.first, SVPicker.sv.second)
+        }
+
+        HuePicker.onPick = ::updateColor
+        SVPicker.onPick = ::updateColor
+    }
+}
